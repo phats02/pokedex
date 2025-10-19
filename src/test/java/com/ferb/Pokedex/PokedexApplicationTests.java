acf3d4fbca45f9d5e60ff26ffc51048afafb1709
phats02 <phattran.2002.work@gmail.com>
2025-10-19 19:21:39 +0700

    Initial Commit

---
+++ src/test/java/com/ferb/Pokedex/PokedexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferb.Pokedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
